46c54181236d846e7980373ec8930b0e14fbd033
Sadik <guenduez.sadik@web.de>
2024-03-06 14:48:47 +0100

    Eingabefeld + Kästchen mit bitflip implementiert

--- DES.js
+++ DES.js
@@ -5,6 +5,7 @@ funktion erstellen die rechtecke erstellt. (SVG)
 die Kästchen mit einer for schleife zeichnen lassen
 
 */
+let gesamt = [];
 let Links = [];
 let Rechts = [];
 
@@ -23,18 +24,36 @@ function Rechtecke(x,y,breite,hoehe,farbe) {
 }
 
 var anzahl_rechtecke = 64;
-const rechteckBreite = 10;
-const rechteckHoehe = 10;
+const rechteckBreite = 25;
+const rechteckHoehe = 25;
 
 // 64 rechtecke erstellen und in die Arrays packen.
 
 for (let i = 0; i < anzahl_rechtecke; i++) {
     let xPosition = i * (rechteckBreite + 5);
     let xPosition2 = (i-32) * (rechteckBreite+5);
-    if (i < anzahl_rechtecke/2) {Links.push(Rechtecke(xPosition,20,rechteckBreite,rechteckHoehe,"blue"));}
-    else {Rechts.push(Rechtecke(xPosition2,40,rechteckBreite,rechteckHoehe,"blue"));}
-    console.log(xPosition2);
+    if (i < anzahl_rechtecke/2) {gesamt.push(Rechtecke(xPosition,20,rechteckBreite,rechteckHoehe,"blue"));}
+    else {gesamt.push(Rechtecke(xPosition2,60,rechteckBreite,rechteckHoehe,"blue"));}
 }
 
-console.log("Inhalt von Links:", Links[5]);
-console.log("Inhalt von Rechts:", Rechts[5]);
+function aufspalten(){
+for (let i = 0;i < gesamt.length;i++) {
+    if (i < gesamt.length/2) { Links.push(gesamt[i]);} else {Rechts.push(gesamt[i]);}
+}
+}
+var binArray = [];
+function Eingabe_zu_bin(hex)
+{
+    for(let i = 0; i < hex.length;i++){
+    let binary = parseInt(hex[i], 16).toString(2);
+        // Fehlende Nullen voranstellen, um 4 Bits zu erhalten
+        while (binary.length < 4) {
+            binary = "0" + binary;
+        }
+        // Jedes Bit separat zum Array hinzufügen
+        for (let j = 0; j < binary.length; j++) {
+            binArray.push(parseInt(binary[j]));
+        }
+    
+    }
+}
